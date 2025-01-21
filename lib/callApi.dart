8332b3220e81ca1ba08cbc5c90f2cae4540113e4
Dorian <deneuchateldorian205@gmail.com>
2025-01-21 16:39:54 +0100

    API compte

--- lib/callApi.dart
+++ lib/callApi.dart
@@ -4,6 +4,7 @@ import 'package:http/http.dart' as http;
 class Utilisateur {
   static String baseUrl = 'http://10.0.2.2:1234';
 
+  // Connexion de l'utilisateur
   static Future<Map<String, dynamic>> checkUser(
       String email, String password) async {
     try {
@@ -27,6 +28,7 @@ class Utilisateur {
 class Seance {
   static String baseUrl = 'http://10.0.2.2:1234';
 
+  // Récupération des séances de sport
   static Future<List<dynamic>> getSeances() async {
     try {
       var res = await http.get(
@@ -37,7 +39,30 @@ class Seance {
       if (res.statusCode == 200) {
         return jsonDecode(res.body);
       } else {
-        throw Exception('Erreur lors de la récupération des vidéos');
+        throw Exception('Erreur lors de la récupération des séances de sport.');
+      }
+    } catch (err) {
+      throw Exception(err.toString());
+    }
+  }
+}
+
+class Compte {
+  static String baseUrl = 'http://10.0.2.2:1234';
+
+  // Récupération des données du compte
+  static Future<Map<String, dynamic>> getCompteData(String userId) async {
+    try {
+      var res = await http.get(
+        Uri.parse(baseUrl + '/user/getUserById?id=$userId'),
+        headers: {'Content-Type': 'application/json'},
+      );
+
+      if (res.statusCode == 200) {
+        return jsonDecode(res.body);
+      } else {
+        throw Exception(
+            'Erreur lors de la récupération des données du compte.');
       }
     } catch (err) {
       throw Exception(err.toString());
