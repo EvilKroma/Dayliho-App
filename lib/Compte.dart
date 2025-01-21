bcdb41dd26b4d7bb895f3336b43dfb4bc31c1cf7
Dorian <deneuchateldorian205@gmail.com>
2025-01-21 17:35:50 +0100

    Photo de profil

--- lib/Compte.dart
+++ lib/Compte.dart
@@ -26,9 +26,9 @@ class Compte extends StatelessWidget {
                 children: [
                   CircleAvatar(
                     radius: 50,
-                    backgroundImage: NetworkImage(
-                        compteData['profilePictureUrl'] ??
-                            'https://via.placeholder.com/150'),
+                    backgroundImage: NetworkImage(compteData[
+                            'profilePictureUrl'] ??
+                        'https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
                   ),
                   SizedBox(height: 20),
                   Text(
