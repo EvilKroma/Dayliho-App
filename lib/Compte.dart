fee3a11b9c39436a45df2adb43ee47a1110721be
Dorian <deneuchateldorian205@gmail.com>
2025-01-21 17:15:57 +0100

    Récupération de l'id

--- lib/Compte.dart
+++ lib/Compte.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 
 class Compte extends StatelessWidget {
-  const Compte({super.key, required this.connectedUserData});
-
   final Map<String, dynamic> connectedUserData;
+  const Compte({super.key, required this.connectedUserData});
 
   @override
   Widget build(BuildContext context) {
+    // Récupération de l'ID de l'utilisateur
+    var userId = connectedUserData['userId'];
+
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text("Data: $connectedUserData"),
+          Text("User ID: $userId"),
         ],
       ),
     );
